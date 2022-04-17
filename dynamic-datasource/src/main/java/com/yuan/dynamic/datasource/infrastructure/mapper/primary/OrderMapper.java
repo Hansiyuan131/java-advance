e8c08ecffd9739170c521d3500c24f8d0d34c62a
Hansiyuan131 <422295068@qq.com>
2022-04-17 17:37:20 +0800

    yuan,docs: 实现批量插入数据

--- dynamic-datasource/src/main/java/com/yuan/dynamic/datasource/infrastructure/mapper/primary/OrderMapper.java
+++ dynamic-datasource/src/main/java/com/yuan/dynamic/datasource/infrastructure/mapper/primary/OrderMapper.java
@@ -3,6 +3,8 @@ package com.yuan.dynamic.datasource.infrastructure.mapper.primary;
 import com.yuan.dynamic.datasource.infrastructure.po.Order;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author hansiyuan
  * @date 2022年04月17日 15:45
@@ -15,4 +17,12 @@ public interface OrderMapper {
      * @param req 入参
      */
     long insert(Order req);
+
+    /**
+     * 批量插入
+     *
+     * @param orderList 请求列表
+     * @return 响应
+     */
+    long batchInsert(List<Order> orderList);
 }
--- dynamic-datasource/src/test/java/com/yuan/dynamic/datasource/infrastructure/mapper/OrderMapperTestInfo.java
+++ dynamic-datasource/src/test/java/com/yuan/dynamic/datasource/infrastructure/mapper/OrderMapperTestInfo.java
@@ -1,19 +1,28 @@
 package com.yuan.dynamic.datasource.infrastructure.mapper;
 
+import com.alibaba.fastjson.JSON;
 import com.yuan.dynamic.datasource.infrastructure.mapper.primary.OrderMapper;
 import com.yuan.dynamic.datasource.infrastructure.po.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.Stopwatch;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author hansiyuan
  * @date 2022年04月17日 15:55
  */
 @SpringBootTest
+@Slf4j
 public class OrderMapperTestInfo {
+
+    private static final int CORE = Math.min(Runtime.getRuntime().availableProcessors() * 2, 16);
 
     @Resource
     private OrderMapper orderMapper;
@@ -22,6 +31,25 @@ public class OrderMapperTestInfo {
     public void insert() {
         Order order = builderOrderInfo();
         System.out.println(orderMapper.insert(order));
+    }
+
+    @Test
+    public void bitchInsert() {
+        StopWatch stopWatch = new StopWatch("batchInsert");
+        stopWatch.start();
+        List<Order> orderList = builderOrderList();
+        Long rows = orderMapper.batchInsert(orderList);
+        log.info("插入条数：{}", rows);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+    }
+
+    private List<Order> builderOrderList() {
+        List<Order> order = new ArrayList<Order>();
+        for (int i = 0; i < 28000; i++) {
+            order.add(builderOrderInfo());
+        }
+        return order;
     }
 
     private Order builderOrderInfo() {
